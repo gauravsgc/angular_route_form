3bbf1fc9d7e5daa14fbaa81ca8c52375118a2bf2
gauravsgc <pgauravpandeydit@gmail.com>
2024-11-07 07:41:10 +0530

    updated

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,16 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule,CommonModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
   constructor(private router: Router){
 
+  }
+  loginForm=new FormGroup({
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required])
+  }
+  )
+  submitHandler(){
+    console.log('Form Submitted', this.loginForm.value);
   }
   buttonHandler(){
     // alert('button clicked');
