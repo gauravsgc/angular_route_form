4b9905e5ccb1d277312a7333ac4e03e55b82a4e4
gauravsgc <pgauravpandeydit@gmail.com>
2024-11-18 08:16:46 +0530

    updated

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,13 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-company',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './company.component.html',
   styleUrl: './company.component.css'
 })
 export class CompanyComponent {
+  info:any=[];
+  //dependency injection
+constructor(private http:HttpClient){
+
+}
+getData(){
+  //console.log('api will call');
+  this.http.get(`https://jsonplaceholder.typicode.com/posts`).subscribe(
+    (result) => {
+      console.log(result);
+      
+      
+      this.info = result;
+      // console.log(this.info[1].title)
+    }
+  );
+}
+
 
 }
